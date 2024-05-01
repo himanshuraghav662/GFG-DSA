2655636843f5df14549064f7042455e2a35bbb75
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2024-05-01 21:17:48 +0530

    Create missing number in array .java

---
+++ missing number in array .java
@@ -0,0 +1,11 @@
+class Solution {
+    int missingNumber(int array[], int n) {
+        // Your Code Here
+        Arrays.sort(array);
+        for(int i=0;i<n-1;i++){
+            if(array[i]!=(i+1))
+            return i+1;
+        }
+        return array[n-2]+1;
+    }
+}
