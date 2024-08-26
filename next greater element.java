ffbd4f7de2e601ca174955a5ed577ca757242d69
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2024-08-26 22:29:28 +0530

    Update next greater element.java

--- next greater element.java
+++ next greater element.java
@@ -1,24 +1,24 @@
 TLE  .........O (N^2)
 
-  class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            int curr=-2;
-            for(int j=0;j<nums2.length;j++){
-             if(nums1[i]==nums2[j]){
-                 curr=nums1[i];
-             }
-             if(curr!=-2){
-                 if(nums2[j]>curr){
-                     ans[i]=nums2[j];
-                     curr=-2;
-                 }
-             }
-             if(j==nums2.length-1 && curr!=-2)
-             ans[i]=-1;
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        long[] a=new long[n];
+        for(int i=0;i<n;i++){
+            int flag=0;
+            for(int j=i;j<n;j++){
+                if(arr[j]>arr[i]){
+                    a[i]=arr[j];
+                    flag=1;
+                     break;
+                }
             }
+            if(flag==0)
+            a[i]=-1;
+        
         }
-        return ans;
-    }
+        return a;
+    } 
 }
