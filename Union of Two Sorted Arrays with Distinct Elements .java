02e3dd384d85c9af1f533e94d5316c3ddc069138
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2024-11-11 00:03:50 +0530

    Update Union of Two Sorted Arrays with Distinct Elements .java

--- Union of Two Sorted Arrays with Distinct Elements .java
+++ Union of Two Sorted Arrays with Distinct Elements .java
@@ -18,3 +18,36 @@ class Solution {
         return ans;
     }
 }
+
+
+////better time complexity//////
+
+class Solution {
+    // Function to return a list containing the union of the two arrays.
+    public static ArrayList<Integer> findUnion(int a[], int b[]) {
+        // add your code here
+        ArrayList<Integer> list = new ArrayList<>();
+    int i=0;
+    int j=0;
+    while(i<a.length || j<b.length){
+        if(i==a.length){
+            list.add(b[j]);
+            j++;
+        }else if(j==b.length){
+            list.add(a[i]);
+            i++;
+        } else if(a[i]==b[j]){
+            list.add(a[i]);
+            i++;
+            j++;
+        }else if(a[i]<b[j]){
+            list.add(a[i]);
+            i++;
+        }else{
+            list.add(b[j]);
+            j++;
+        }
+    }
+    return list;
+    }
+}
