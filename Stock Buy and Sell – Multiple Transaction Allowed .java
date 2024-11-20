b05113bab47cb97788411ade5a643b793a5e8cb3
himanshuraghav662 <108710259+himanshuraghav662@users.noreply.github.com>
2024-11-21 01:38:57 +0530

    Create Stock Buy and Sell – Multiple Transaction Allowed .java

---
+++ Stock Buy and Sell – Multiple Transaction Allowed .java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumProfit(int arr[]) {
+        // code here
+          int n = arr.length;
+        if (n < 2) return 0; 
+        
+        int profit = 0;
+        
+        for (int i = 1; i < n; i++) {
+    
+            if (arr[i] > arr[i - 1]) {
+                profit += arr[i] - arr[i - 1];
+            }
+        }
+        
+        return profit;
+    }
+}
